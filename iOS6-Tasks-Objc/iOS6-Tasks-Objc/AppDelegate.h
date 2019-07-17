6a4f3919c84906a2f49784885d4214c24e4ab95c
Paul Solt <PaulSolt@gmail.com>
2019-07-17 12:18:37 -0400

    Added project

---
+++ iOS6-Tasks-Objc/iOS6-Tasks-Objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS6-Tasks-Objc
+//
+//  Created by Paul Solt on 7/17/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS6-Tasks-Objc/iOS6-Tasks-Objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS6-Tasks-Objc
+//
+//  Created by Paul Solt on 7/17/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
